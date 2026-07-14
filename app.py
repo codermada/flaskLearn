0a38074800c577440f243137f0a48ec218adc68a
codermada <tolotra025@gmail.com>
2026-07-14 21:40:09 +0300

    custom error pages

--- app.py
+++ app.py
@@ -17,5 +17,13 @@ def contact():
 def bootstrap():
     return render_template("bootstrap.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
