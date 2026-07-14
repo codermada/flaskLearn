344dbe3fa93d75d51bf83dd058428524e235e877
codermada <tolotra025@gmail.com>
2026-07-14 21:08:46 +0300

    bootstrap and popper.js

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ app = Flask(__name__, template_folder="bootstrap_templates", static_folder="boot
 def hello():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("contact.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/bootstrap')
+def bootstrap():
+    return render_template("bootstrap.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
